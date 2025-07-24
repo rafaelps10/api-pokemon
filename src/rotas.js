bf5e76ac5696a3a94e8455e3f8a975f0bcd60391
Bowie <rafael.ps.skt@gmail.com>
2025-07-24 17:36:25 -0300

    feat: listar e detalhar pokemons

---
+++ src/rotas.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const { pokemons, pokemon } = require('./controladores/pokemons');
+
+const rotas = express();
+
+rotas.get('/pokemon', pokemons);
+rotas.get('/pokemon/:idOuNome', pokemon);
+
+
+module.exports = rotas;
