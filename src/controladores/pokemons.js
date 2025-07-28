d39087a4a76a25b7f799334ca7231a58d62757a0
Bowie <rafael.ps.skt@gmail.com>
2025-07-28 20:41:04 -0300

    fix(pokemon): adiciona try/catch nas funções de listagem e detalhamento

--- src/controladores/pokemons.js
+++ src/controladores/pokemons.js
@@ -3,19 +3,27 @@ const { listarPokemons, detalharPokemon } = require('utils-playground');
 const pokemons = async (req, res) => {
     const { pagina } = req.query;
 
-    const listaPokemons = await listarPokemons(pagina ?? 1);
+    try {
+        const listaPokemons = await listarPokemons(pagina ?? 1);
     
-    return res.json(listaPokemons);
+        return res.json(listaPokemons);
+    } catch (error) {
+        return res.status(400).json({ mensagem: error.message });
+    }
 }
 
 const pokemon = async (req, res) => {
     const { idOuNome } = req.params;
 
-    const pokemonEncontrado = await detalharPokemon(idOuNome);
+    try {
+        const pokemonEncontrado = await detalharPokemon(idOuNome);
 
-    const { id, name, height, weight, base_experience, forms, abilities, species } = pokemonEncontrado;
+        const { id, name, height, weight, base_experience, forms, abilities, species } = pokemonEncontrado;
 
-    return res.json({ id, name, height, weight, base_experience, forms, abilities, species });
+        return res.json({ id, name, height, weight, base_experience, forms, abilities, species });
+    } catch (error) {
+        return res.status(400).json({ mensagem: error.message})
+    }
 
 }
 
